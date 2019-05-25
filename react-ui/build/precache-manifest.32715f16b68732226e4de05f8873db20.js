8aa26fd9c95aac5c517176e25bd84c8821c5415b
Igor Schouten <igor@igorschouten.com>
2019-05-25 08:48:15 +0200

    quickfix: include react build directly

---
+++ react-ui/build/precache-manifest.32715f16b68732226e4de05f8873db20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c4cfbb9edce5ddbafe05b05aae7324",
+    "url": "/index.html"
+  },
+  {
+    "revision": "578213784b407f4608dc",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2696a5db408d4b1f0009",
+    "url": "/static/js/2.eb1964f0.chunk.js"
+  },
+  {
+    "revision": "578213784b407f4608dc",
+    "url": "/static/js/main.502c3034.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
